a7b44bc17a98820a5574912b56a5809cb962b005
Kailyev <karlaeef@gmail.com>
2021-06-06 22:55:11 -0500

    Sesión 29

---
+++ server/config/winston.js
@@ -0,0 +1,79 @@
+// Importando a Winston
+import winston, { format } from 'winston';
+import appRoot from 'app-root-path';
+
+// Componentes para crear el formato personalizado
+const { combine, timestamp, printf, uncolorize, jason, colorize } = format;
+// Creando el perfil de color para el log
+const colors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'magenta',
+  debug: 'green',
+};
+// Agregando el perfil a winston
+winston.addColors(colors);
+
+// Formato de consola
+const myFormat = combine(
+  colorize({ all: true }),
+  timestamp(),
+  printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
+);
+
+// Formato para la salida de los archivos de log
+const myFileFormat = combine(uncolorize(), timestamp(), jason());
+
+// Creando objetos de configuración
+const options = {
+  infoFile: {
+    level: 'info',
+    filename: `${appRoot}/server/logs/infos.log`,
+    handleExceptions: true,
+    maxsize: 5242880, //5MB,
+    maxFiles: 5,
+    format: myFileFormat,
+  },
+  warnFile: {
+    level: 'warn',
+    filename: `${appRoot}/server/logs/warns.log`,
+    handleExceptions: true,
+    maxsize: 5242880, //5MB,
+    maxFiles: 5,
+    format: myFileFormat,
+  },
+  errorFile: {
+    level: 'error',
+    filename: `${appRoot}/server/logs/errors.log`,
+    handleExceptions: true,
+    maxsize: 5242880, //5MB,
+    maxFiles: 5,
+    format: myFileFormat,
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    format: myFileFormat,
+  },
+};
+
+// Creando la instancia del logger
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File(options.infoFile),
+    new winston.transports.File(options.warnFile),
+    new winston.transports.File(options.errorFile),
+    new winston.transports.Console(options.console),
+  ],
+  exitOnError: false, // no finaliza en excepciones manejadas
+});
+
+// Manejo de un stram de entrada
+logger.stream = {
+  write(message) {
+    logger.info(message);
+  },
+};
+
+export default logger;
