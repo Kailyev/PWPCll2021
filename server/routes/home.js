27d6f8c2a57cbba764bc31be97f222763c14e5e8
Kailyev <karlaeef@gmail.com>
2021-06-30 20:08:09 -0500

    sesión 34

--- server/routes/home.js
+++ server/routes/home.js
@@ -8,10 +8,13 @@ import homeController from '@server/controllers/homeController';
 const router = new Router();
 
 // GET '/'
-router.get('/', homeController.index);
+router.get(['/', '/index'], homeController.index);
 
 // Get '/gretting
 router.get('/greeting', homeController.greeting);
+
+// Get '/about'
+router.get('/about', homeController.about);
 
 // Exportando el router que maneja las subrutas
 // para el controlador home
